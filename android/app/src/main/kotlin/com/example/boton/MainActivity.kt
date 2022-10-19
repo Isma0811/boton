7809948af38f418557a32e443c725467fe7bb32e
Isma0811 <jesusismaelvillanueva@gmail.com>
2022-10-19 13:03:48 -0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/boton/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boton
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Hola mundo',
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Material App Bar'),
+          ),
+          body: Center(
+            child: ElevatedButton(
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.amber)),
+              child: const Text("boton trabajo 1"),
+              onPressed: () {},
+            ),
+          ),
+        ));
+  }
+}
